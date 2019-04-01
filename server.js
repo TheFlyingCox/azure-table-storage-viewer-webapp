73e180dece9ee892c8115287a87b069872bdf1d3
Cox <dcox@cgifederal.com>
2019-04-01 17:02:13 -0500

    syntax fixin8

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ server.register(require('inert'), (err) => {
   });
 });
 
-Handlebars.registerHelper('ifTrue', text => text === 'True' ? "style='background-color: green;'" : "style='background-color: inherit;'");
+Handlebars.registerHelper('ifTrue', text => text === 'True' ? "background-color: green;" : "background-color: inherit;");
 
 server.register(require('vision'), (err) => {
 
