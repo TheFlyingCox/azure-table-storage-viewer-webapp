66121d4da938fa7a1517f018576cca21e3d773d0
TheFlyingCox <dandanthis@gmail.com>
2019-04-02 13:05:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ server.register(require('inert'), (err) => {
   });
 });
 
-Handlebars.registerHelper('ifTrue', {text => text === 'True' ? "background-color: green;" : text => text === 'Error' ? "background-color: red;" : "background-color: inherit;"});
+Handlebars.registerHelper('ifTrue', text => text === 'True' ? "background-color: green;" : "background-color: inherit;");
+#Handlebars.registerHelper('ifTrue', {text => text === 'True' ? "background-color: green;" : text => text === 'Error' ? "background-color: red;" : "background-color: inherit;"});
 
 server.register(require('vision'), (err) => {
 
