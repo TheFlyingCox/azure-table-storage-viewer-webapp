4137f77589cf4dd4dfb528ca14a717d8188eb4ce
Cox <dcox@cgifederal.com>
2019-04-03 00:03:39 -0500

    TestTime

--- server.js
+++ server.js
@@ -32,11 +32,8 @@ server.register(require('inert'), (err) => {
 });
 
 Handlebars.registerHelper('ifTrue', text => text === 'True' ? "background-color: #66ff66;" : text === 'N/A' ? "background-color: #66ff66;" : text === 'Error' ? "background-color: #ff3737;" : "background-color: inherit;");
-Handlebars.registerHelper('tFormat', time => {
-  const t = New Date(time);
-  t.toLocaleString();
-});
-                          
+Handlebars.registerHelper('tFormat', time => time.toLocaleString());
+
 server.register(require('vision'), (err) => {
 
   Hoek.assert(!err, err);
